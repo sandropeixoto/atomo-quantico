919c05b3337996f7e4e98f6d9dc8817a4c4c0238
belemonline <belemonline@gmail.com>
2026-02-09 20:25:34 +0000

    fix UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#F0F4F8',
-        secondary: '#FFDAB9',
-        accent: '#E6E6FA',
-        'text-primary': '#333333',
-        'text-secondary': '#555555',
-        background: '#FFFFFF',
-      },
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      },
+    colors: {
+      primary: '#F0F4F8',
+      secondary: '#FFDAB9',
+      accent: '#E6E6FA',
+      'text-primary': '#333333',
+      'text-secondary': '#555555',
+      background: '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
     },
   },
   plugins: [],
