69eb932f29989bdfd02d82332b07fec4d0439681
belemonline <belemonline@gmail.com>
2026-02-10 12:09:15 +0000

    fix UI

--- src/components/AtomoQuanticoLogo.tsx
+++ src/components/AtomoQuanticoLogo.tsx
@@ -1,16 +1,9 @@
 export function AtomoQuanticoLogo() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8 text-secondary"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-    </svg>
+    <img
+      src="/atomoquantico235.svg"
+      alt="Átomo Quântico Logo"
+      className="h-8 w-8"
+    />
   );
 }
