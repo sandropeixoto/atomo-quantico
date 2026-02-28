4288ace128baff224cca79a8f057b7d08f80518b
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-02-27 22:29:38 -0300

    feat: add android support with capacitor and firebase integration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nano.atomoquantico',
+  appName: 'Átomo Quântico',
+  webDir: 'dist'
+};
+
+export default config;
